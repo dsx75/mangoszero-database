3a0820cdde18c546324474ff8a8dffba3b5ace90
Dominik Smatana <taidana@kage.sk>
2022-03-20 13:33:05 +0100

    FK auction --> characters
    Added a foreign key: Auction.ItemOwner --> Characters.GUID
    
    For testing purposes.

--- Character/Integrity test/FK_test_for_Rel 22_2_1.sql
+++ Character/Integrity test/FK_test_for_Rel 22_2_1.sql
@@ -3,3 +3,9 @@
 
 ALTER TABLE `auction`
   ADD CONSTRAINT `fk_auction_iteminstance` FOREIGN KEY (`itemguid`) REFERENCES `item_instance` (`guid`) ON UPDATE CASCADE;
+
+
+/* Auction.ItemOwner --> Characters.GUID */
+
+ALTER TABLE `auction`
+  ADD CONSTRAINT `fk_auction_characters` FOREIGN KEY (`itemowner`) REFERENCES `characters` (`guid`) ON UPDATE CASCADE;
